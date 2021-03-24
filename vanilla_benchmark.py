f41c6092d50e6e5776554f3360c32d6c607042fc
AlexTMallen <atmallen8@gmail.com>
2021-03-23 19:57:23 -0700

    vanilla benchmark updates

--- vanilla_benchmark.py
+++ vanilla_benchmark.py
@@ -3,6 +3,7 @@
 
 import json
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 
@@ -20,100 +21,96 @@ def pinball_loss(data, quant_preds, quantiles):
     return loss / (len(data) * len(quantiles))
 
 
-def test(all_demand, hours_of_day, all_temps, months_of_year, train_through=None, gap=0, test_through=None, plot=False):
-    days_of_week = np.array(list((i // 24) % 7 for i in range(len(all_demand))), dtype=np.uint8)
+def test(all_df, train_start, train_through, test_length, gap=0, temp_years=None, plot=False):
+    variables = pd.DataFrame(all_df.temp)
+    variables["temp2"] = variables.temp ** 2
+    variables["temp3"] = variables.temp ** 3
+    variables["trend"] = variables.index
+
+    for i in range(2, 13):
+        variables[f"m{i}"] = (i == all_df.month) * 1  # converts to int
+        variables[f"tempmonth{i}"] = variables.temp * variables[f"m{i}"]
+        variables[f"temp2month{i}"] = variables.temp2 * variables[f"m{i}"]
+        variables[f"temp3month{i}"] = variables.temp3 * variables[f"m{i}"]
+
+    for i in range(2, 25):
+        variables[f"h{i}"] = (i == all_df.hour_end) * 1
+        variables[f"temphour{i}"] = variables.temp * variables[f"h{i}"]
+        variables[f"temp2hour{i}"] = variables.temp2 * variables[f"h{i}"]
+        variables[f"temp3hour{i}"] = variables.temp3 * variables[f"h{i}"]
+
+    for i in range(1, 7):
+        variables[f"w{i}"] = (i == all_df.day_of_week) * 1
+
+    for i in range(1, 7):
+        for j in range(2, 25):
+            variables[f"wh{i}_{j}"] = variables[f"w{i}"] * variables[f"h{j}"]
 
     cap = train_through
-    demand = all_demand[:cap]
-    hour = np.zeros((23, cap), dtype=np.uint8)
-    for i in range(1, 24):
-        hour[i - 1] = (i == hours_of_day[:cap])
-    week = np.zeros((6, cap), dtype=np.uint8)
-    for i in range(6):
-        week[i] = (i == days_of_week[:cap])
-    month = np.zeros((11, cap), dtype=np.uint8)
-    for i in range(1, 12):
-        month[i - 1] = (i == months_of_year[:cap])
-    trend = np.arange(cap)
-    weekhour = np.zeros((6 * 23, cap))
-    for i in range(6):
-        for j in range(23):
-            weekhour[23 * i + j] = week[i] * hour[j]
-    temp = all_temps[-len(all_demand):][:cap]
-    temp2 = temp ** 2
-    temp3 = temp ** 3
-    tempmonth = np.array(list(map(lambda x: temp * x, month)))
-    temp2month = np.array(list(map(lambda x: temp2 * x, month)))
-    temp3month = np.array(list(map(lambda x: temp3 * x, month)))
-    temphour = np.array(list(map(lambda x: temp * x, hour)))
-    temp2hour = np.array(list(map(lambda x: temp2 * x, hour)))
-    temp3hour = np.array(list(map(lambda x: temp3 * x, hour)))
+    train_df = all_df.iloc[train_start:cap]
+    train_vars = variables.iloc[train_start:cap]
 
     reg = linear_model.LinearRegression()
 
-    variables = [trend, *month, *week, *hour, *weekhour, temp, temp2, temp3, *tempmonth, *temp2month, *temp3month,
-                 *temphour, *temp2hour, *temp3hour]
-    variables = list(map(list, zip(*variables)))  # transpose
-    reg.fit(variables, demand)
+    reg.fit(train_vars, train_df.load)
 
     if plot:
         fit = reg.predict(variables)
-        plt.figure()
-        plt.plot(demand)
+        plt.plot(all_df.load.values)
         plt.plot(fit)
+        plt.axvline(train_through, color="k")
         plt.show()
 
     #  ###---TEST---###
-    slc = test_through
-    cap = train_through + slc + gap
-    test_demand = all_demand[cap - slc:cap]
-    hour = np.zeros((23, len(test_demand)), dtype=np.uint8)
-    for i in range(1, 24):
-        hour[i - 1] = (i == hours_of_day[cap - slc:cap])
-    week = np.zeros((6, len(test_demand)), dtype=np.uint8)
-    for i in range(6):
-        week[i] = (i == days_of_week[cap - slc:cap])
-    month = np.zeros((11, len(test_demand)), dtype=np.uint8)
-    for i in range(1, 12):
-        month[i - 1] = (i == months_of_year[cap - slc:cap])
+    start = train_through + gap
+    cap = train_through + test_length + gap
+    test_demand = all_df.load.iloc[start:cap]
 
-    weekhour = np.zeros((6 * 23, len(test_demand)))
-    for i in range(6):
-        for j in range(23):
-            weekhour[23 * i + j] = week[i] * hour[j]
-
-    trend = np.arange(slc, slc + len(demand))
     temps = []
-    num_years = train_through / (24 * 365.24)
+    years = np.unique(train_df.year) if temp_years is None else temp_years
+    num_years = len(years)
     k = int((99 / num_years - 1) / 2 + 0.5)
     print("k =", k)
-    for yr in range(int(num_years + 0.5)):
-        for shift in range(2*k + 1):
-            offset = int((shift + int(yr * 365.24 + 0.5)) * 24 + 0.5)
-            temps.append(all_temps[-len(all_demand) - k * 24:][offset + gap: offset + slc + gap])
+    for yr in years:
+        this_year = all_df.iloc[:train_through][all_df.year.iloc[:train_through] == yr]
+        for shift in range(-k, k + 1):
+            delta = pd.Timedelta(days=shift)
+            dates = all_df.date.iloc[start:cap] + delta
+            test_days_of_year = np.unique(list(date.timetuple().tm_yday for date in dates))
+            include = list(day in test_days_of_year for day in this_year.day_of_year)
+            temps.append(this_year.temp[include].values)
+
+    if plot:
+        plt.plot(np.array(temps).T, linewidth=0.5)
+        plt.plot(variables.temp[start:cap].values, linewidth=2, color="orange")
+        plt.show()
 
     preds = []
-    for temp in temps:
-        temp2 = temp ** 2
-        temp3 = temp ** 3
-        tempmonth = np.array(list(map(lambda x: temp * x, month)))
-        temp2month = np.array(list(map(lambda x: temp2 * x, month)))
-        temp3month = np.array(list(map(lambda x: temp3 * x, month)))
-        temphour = np.array(list(map(lambda x: temp * x, hour)))
-        temp2hour = np.array(list(map(lambda x: temp2 * x, hour)))
-        temp3hour = np.array(list(map(lambda x: temp3 * x, hour)))
-        test_variables = [trend, *month, *week, *hour, *weekhour, temp, temp2, temp3, *tempmonth, *temp2month, *temp3month,
-                          *temphour, *temp2hour, *temp3hour]
-        test_variables = list(map(list, zip(*test_variables)))  # transpose
-        preds.append(reg.predict(test_variables))
+    for i, temp in enumerate(temps):
+        test_vars = variables.iloc[start:cap].copy()
+
+        test_vars.loc[:, "temp"] = temp
+        test_vars.loc[:, "temp2"] = temp ** 2
+        test_vars.loc[:, "temp3"] = temp ** 3
+
+        for i in range(2, 13):
+            test_vars.loc[:, f"tempmonth{i}"] = test_vars.temp * test_vars[f"m{i}"]
+            test_vars.loc[:, f"temp2month{i}"] = test_vars.temp2 * test_vars[f"m{i}"]
+            test_vars.loc[:, f"temp3month{i}"] = test_vars.temp3 * test_vars[f"m{i}"]
+
+        for i in range(2, 25):
+            test_vars.loc[:, f"temphour{i}"] = test_vars.temp * test_vars[f"h{i}"]
+            test_vars.loc[:, f"temp2hour{i}"] = test_vars.temp2 * test_vars[f"h{i}"]
+            test_vars.loc[:, f"temp3hour{i}"] = test_vars.temp3 * test_vars[f"h{i}"]
+
+        preds.append(reg.predict(test_vars))
 
     preds = np.array(preds)
 
     if plot:
         pred = np.mean(preds, axis=0)
         std = np.std(preds, axis=0)
-        plt.figure()
-        plt.plot(test_demand)
+        plt.plot(test_demand.values)
         plt.plot(pred)
         plt.plot(std + pred, linestyle="--", color="black", linewidth=0.5)
         plt.plot(pred - std, linestyle="--", color="black", linewidth=0.5)
@@ -129,10 +126,10 @@ def test(all_demand, hours_of_day, all_temps, months_of_year, train_through=None
             quant_pred.append(sorted_preds[idx])
         quant_preds.append(quant_pred)
 
-    return pinball_loss(test_demand, quant_preds, quantiles)
+    return pinball_loss(test_demand.values, quant_preds, quantiles)
 
 
-def get_lossesGEFCom(start=0, zones=None, plot=False, train_through=365*24, delay=0):
+def get_lossesGEFCom(train_through_years, test_length, zones=None, start_date=None, delay_days=0, temp_years=None, plot=False):
     with open("GEFCom2017//GEFCom2017-Qual//GEFCom2017Qual2005-2015.json", "r") as f:
         alldata = json.loads(f.read())
 
@@ -140,23 +137,28 @@ def get_lossesGEFCom(start=0, zones=None, plot=False, train_through=365*24, dela
     zones = alldata.keys() if zones is None else zones
     for zone_name in zones:
         print(zone_name)
-        hours_of_day = np.array(alldata[zone_name]["Hour"], dtype=np.uint8)[start:]
-        demand = np.array(alldata[zone_name]["DEMAND"], dtype=np.float64)[start:]
-        all_temps = np.array(alldata[zone_name]["DryBulb"], dtype=np.float64)[start % (365 * 24):]
-        if start == 0:
-            prefix = np.tile(all_temps[:24], 4)
-            all_temps = np.concatenate([prefix, all_temps])
-        print("start:", alldata[zone_name]["Date"][start])
-        months = np.array(list(map(lambda x: int(x[5:7]), alldata[zone_name]["Date"])), dtype=np.uint8)[start:]
+        zone_data = alldata[zone_name]
+        all_df = pd.DataFrame({"date": zone_data["Date"], "load": zone_data["DEMAND"], "temp": zone_data["DryBulb"]})
+        all_df.date = pd.to_datetime(all_df.date)
+        all_df["day_of_week"] = list((i // 24) % 7 for i in range(len(all_df)))
+        all_df["hour_end"] = list(i % 24 + 1 for i in range(len(all_df)))
+        all_df["month"] = list(all_df.date.loc[i].month for i in all_df.index)
+        all_df["day_of_year"] = list(date.timetuple().tm_yday for date in all_df.date)
+        all_df["year"] = list(date.timetuple().tm_year for date in all_df.date)
 
-        length = len(demand)
-        losses[zone_name] = test(demand, hours_of_day, all_temps, months, train_through=train_through, gap=delay, test_through=31*24, plot=plot)
+        bad_idxs = all_df.load.isnull().to_numpy().nonzero()[0]
+        all_df.loc[bad_idxs, "load"] = (all_df.load.values[bad_idxs - 1] + all_df.load.values[bad_idxs + 1]) / 2
 
+        start_date = all_df.date.iloc[0] if start_date is None else start_date
+        train_start = all_df.index[start_date == all_df.date][0]
+        train_through = all_df[all_df.date == start_date.replace(year=start_date.year + train_through_years)].index[0]
+        losses[zone_name] = test(all_df, train_start, train_through, test_length, gap=delay_days * 24, temp_years=temp_years, plot=plot)
     print(losses)
     return losses
 
 
 if __name__ == "__main__":
     # losses = get_lossesGEFCom(start=9 * 365 * 24 + 2 * 24 + 31 * 24, plot=True)
-    losses = get_lossesGEFCom(start=int(9 * 24 * 365 / 12), delay=52 * 24,  train_through=(10 * 365 + 2) * 24, zones=["ISONE CA"], plot=True)
+    losses = get_lossesGEFCom(10, 31 * 24, start_date=pd.Timestamp("2005-10-01"), zones=["ISONE CA"],
+                              delay_days=0, temp_years=[2014], plot=True)
     print(losses)
