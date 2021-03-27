fixed numerical instability in skew

koopman_probabilistic.py
@@ -259,7 +259,7 @@ class KoopmanProb(nn.Module):
 
         return E, E_ft
 
-    def sgd(self, xt, weight_decay=0, verbose=False, lr_theta=1e-5, lr_omega=1e-5, num_slices=None):
+    def sgd(self, xt, weight_decay=0, verbose=False, lr_theta=1e-5, lr_omega=1e-5, training_mask=None):
         '''
 
         sgd performs a single epoch of stochastic gradient descent on parameters
@@ -293,21 +293,12 @@ class KoopmanProb(nn.Module):
         T = xt.shape[0]
         t = torch.arange(T, device=self.device)
 
-        training_mask = None
-        if num_slices is not None:
-            slice_width = T // num_slices
-            training_mask = torch.zeros(T, device=self.device)
-            for slc in range(num_slices):
-#                 if slc % 2 == iteration % 2:
-                if slc % num_slices != 0:
-                    training_mask[slc * slice_width: (slc + 1) * slice_width] = 1
-            print("training_mask:", training_mask[::slice_width // 2], training_mask.shape)
-
         losses = []
 
-        idxs = np.arange(len(t))
+        # create random batches
+        idxs = np.arange(T)
         np.random.shuffle(idxs)
-        batches = idxs[:len(t) // batch_size * batch_size].reshape((len(t) // batch_size, batch_size))
+        batches = idxs[:T // batch_size * batch_size].reshape((T // batch_size, batch_size))
 
         for i in range(len(batches)):
 
@@ -347,7 +338,8 @@ class KoopmanProb(nn.Module):
 
         return np.mean(losses)
 
-    def fit(self, xt, iterations=10, interval=5, cutoff=np.inf, weight_decay=0, verbose=False, lr_theta=1e-5, lr_omega=1e-5, num_slices=None):
+    def fit(self, xt, iterations=10, interval=5, cutoff=np.inf, weight_decay=0, verbose=False, lr_theta=1e-5,
+            lr_omega=1e-5, training_mask=None):
         '''
         Given a dataset, this function alternatingly optimizes omega and
         parameters of f. Specifically, the algorithm performs interval many
@@ -369,7 +361,11 @@ class KoopmanProb(nn.Module):
         weight_decay : TYPE float, regularization parameter
         lr_theta : TYPE float, learning rate for the model object
         lr_omega : TYPE float, learning rate for adjusting omegas
-        num_slices : TYPE int, number of slices into which training data should be divided to prevent overfitting
+        training_mask : TYPE torch.tensor, which training data points should be
+                        using in optimizing the location parameter (eg. mu).
+                        shape should match xt. a 1 indicates to train mu on that data,
+                        a 0 indicates not to.
+                        Default: None (equivalent to torch.ones(xt.shape))
 
         Returns
         -------
@@ -393,7 +389,8 @@ class KoopmanProb(nn.Module):
                 print('Iteration ', i)
                 print(2 * np.pi / self.omegas)
 
-            l = self.sgd(xt, weight_decay=weight_decay, verbose=verbose, lr_theta=lr_theta, lr_omega=lr_omega, num_slices=num_slices)
+            l = self.sgd(xt, weight_decay=weight_decay, verbose=verbose, lr_theta=lr_theta, lr_omega=lr_omega,
+                         training_mask=training_mask)
             losses.append(l)
             if verbose:
                 print('Loss: ', l)

main.py
@@ -1,10 +1,24 @@
-from koopman_probabilistic import *
-from model_objs import *
+import random
+import torch
 import numpy as np
 import numpy.matlib
 import matplotlib.pyplot as plt
 import json
 from datetime import datetime
+
+seed = 633
+print("[ Using Seed : ", seed, " ]")
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.cuda.manual_seed(seed)
+numpy.random.seed(seed)
+random.seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+from koopman_probabilistic import *
+from model_objs import *
+
 
 def koopman_main():
     # seed = np.random.randint(1000)
@@ -34,9 +48,11 @@ def koopman_main():
     x = data[train_start:]
     x = x[:predict_through]
     xt = x[:train_through]
+    mask = torch.ones(xt.shape, dtype=torch.uint8)
+    mask[:1000] = 0
 
     now = ("_".join(str(datetime.now()).split())).replace(":", ".")
-    data_name = "EXPnormality_" + f"_train_start={train_start}_" + now
+    data_name = "main_" + f"_train_start={train_start}_" + now
 
     num_freqs = [5, 5, 5]
     num_fourier = 4
@@ -55,8 +71,8 @@ def koopman_main():
 
     # k.fit(xt, iterations=20, interval=10, verbose=False, cutoff=0, weight_decay=1e-10000, lr_theta=5e-4, lr_omega=0,
     #       num_slices=None)
-    k.fit(xt, iterations=50, interval=10, verbose=True, cutoff=1, weight_decay=0, lr_theta=1e-4, lr_omega=0,
-          num_slices=None)
+    k.fit(xt, iterations=150, interval=10, verbose=True, cutoff=0, weight_decay=0, lr_theta=1e-4, lr_omega=0,
+          training_mask=mask)
 
     ### FORECAST ###
     params = k.predict(predict_through)

model_objs.py
@@ -143,7 +143,7 @@ class SkewNLL(ModelObject):
     def forward(self, w, data, training_mask):
         y, z, a = self.decode(w)
         norm = torch.distributions.normal.Normal(0, 1)
-        return -torch.mean((-(data - y) ** 2 / (2 * z ** 2)) - z.log() + norm.cdf(a * (data - y) / abs(z)).log(),
+        return -torch.mean((-(data - y) ** 2 / (2 * z ** 2)) - z.log() + norm.cdf(a * (data - y) / z).log(),
                            dim=-1)
 
     def mean(self, params):
@@ -162,13 +162,11 @@ class AlternatingSkewNLL(ModelObject):
     def __init__(self, x_dim, num_freqs, n):
         """
         neural network that takes a vector of sines and cosines and produces a skew-normal distribution with parameters
-        mu, sigma, and alpha (the outputs of the NN). trains using NLL and trains mu and sigma separately to prevent
-        overfitting
+        mu, sigma, and alpha (the outputs of the NN). trains using NLL. reserves some of the training data to only train
+        sigma and alpha to mitigate overconfidence
         :param x_dim: number of dimensions spanned by the probability distr
         :param num_freqs: list. number of frequencies used for each of the 3 parameters: [num_mu, num_sig, num_alpha]
         :param n: size of NN's second layer
-        :param num_slices: training data will be divided into num_slices slices in time, and they will alternate between
-                           being used to train mu and sigma/alpha
         """
         super(AlternatingSkewNLL, self).__init__(num_freqs)
 
@@ -184,6 +182,8 @@ class AlternatingSkewNLL(ModelObject):
         self.l2_a = nn.Linear(n, 32)
         self.l3_a = nn.Linear(32, x_dim)
 
+        self.norm = torch.distributions.normal.Normal(0, 1)
+
     def decode(self, w):
         w_mu = w[..., self.param_idxs[0]]
         y1 = nn.Tanh()(self.l1_mu(w_mu))
@@ -193,7 +193,7 @@ class AlternatingSkewNLL(ModelObject):
         w_sigma = w[..., 2 * self.num_freqs[0]:2 * self.num_freqs[0] + 2 * self.num_freqs[1]]
         z1 = nn.Tanh()(self.l1_sig(w_sigma))
         z2 = nn.Tanh()(self.l2_sig(z1))
-        z = 10 * nn.Softplus()(self.l3_sig(z2))  # std should start big to avoid infinite gradients
+        z = 10 * nn.Softplus()(self.l3_sig(z2))  # start with large uncertainty to avoid small probabilities
 
         w_a = w[..., -2 * self.num_freqs[2]:]
         a1 = nn.Tanh()(self.l1_a(w_a))
@@ -215,12 +215,23 @@ class AlternatingSkewNLL(ModelObject):
             z = sig
             a = alpha
 
-        # avg = torch.mean((data - y) ** 2, dim=-1)
-        # return avg
-        norm = torch.distributions.normal.Normal(0, 1)
-        losses = (-(data - y)**2 / (2 * z**2)) - z.log() + norm.cdf(a * (data - y) / abs(z)).log()
-        avg = -torch.mean(losses, dim=-1)
+        losses = (data - y)**2 / (2 * z**2) + z.log() - self._norm_logcdf(a * (data - y) / z)
+        avg = torch.mean(losses, dim=-1)
         return avg
+
+    def _norm_logcdf(self, z):
+
+        if (z < -7).any():  # these result in NaNs otherwise
+            print("USING LOG CDF APPROXIMATION FOR THIS BATCH TO AVOID FLOATING POINT ERRORS\n\n\n")
+            # https://stats.stackexchange.com/questions/106003/approximation-of-logarithm-of-standard-normal-cdf-for-x0/107548#107548?newreg=5e5f6365aa7046aba1c447e8ae263fec
+            # I found this approx to be good: less than 0.04 error for all -20 < x < -5
+            # approx = lambda x: -0.5 * x ** 2 - 4.8 + 2509 * (x - 13) / ((x - 40) ** 2 * (x - 5))
+            ans = torch.where(z < -0.1, -0.5 * z ** 2 - 4.8 + 2509 * (z - 13) / ((z - 40) ** 2 * (z - 5)),
+                                        -torch.exp(-z * 2) / 2 - torch.exp(-(z - 0.2) ** 2) * 0.2)
+        else:
+            ans = self.norm.cdf(z).log()
+
+        return ans
 
     def mean(self, params):
         mu, sigma, alpha = params