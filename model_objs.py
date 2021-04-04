f0418fa37793632cc2988f8e8050525c37eab2a8
AlexTMallen <atmallen8@gmail.com>
2021-04-03 22:51:34 -0700

    atmo chem tests, gammaNLL

--- model_objs.py
+++ model_objs.py
@@ -16,7 +16,7 @@ class ModelObject(nn.Module):
             self.param_idxs.append(idxs)
             cumul += num_freq
 
-    def forward(self, y, x, training_mask):
+    def forward(self, w, data, training_mask=None):
         """
         Forward computes the error.
 
@@ -32,18 +32,14 @@ class ModelObject(nn.Module):
 
         raise NotImplementedError()
 
-    def decode(self, y):
+    def decode(self, w):
         """
         Evaluates f at temporal snapshots y
 
         Input:
-            y: temporal snapshots of the linear system
+            w: temporal snapshots of the linear system
                 type: torch.tensor
                 dimensions: [T, (batch,) num_frequencies ]
-
-            x: data set
-                type: torch.tensor
-                dimensions: [T, ...]
         """
         raise NotImplementedError()
 
@@ -54,107 +50,6 @@ class ModelObject(nn.Module):
     def std(self, params):
         """returns the standard deviation of a distribution with the given params"""
         return np.ones(params[0].shape)
-
-
-class NormalNLL(ModelObject):
-
-    def __init__(self, x_dim, num_freqs, n):
-        """
-        Negative Log Likelihood neural network assuming Gaussian distribution of x at every point in time
-        :param x_dim: dimension of what will be modeled
-        :param num_freqs: list of the number of frequencies used to model each parameter: [num_mu, num_sigma]
-        :param n: size of 2nd layer of NN
-        """
-        super(NormalNLL, self).__init__(num_freqs)
-
-        self.l1_mu = nn.Linear(2 * self.num_freqs[0], n)
-        self.l2_mu = nn.Linear(n, 64)
-        self.l3_mu = nn.Linear(64, x_dim)
-
-        self.l1_sig = nn.Linear(2 * self.num_freqs[1], n)
-        self.l2_sig = nn.Linear(n, 64)
-        self.l3_sig = nn.Linear(64, x_dim)
-
-    def decode(self, w):
-        w_mu = w[..., self.param_idxs[0]]
-        y1 = nn.Tanh()(self.l1_mu(w_mu))
-        y2 = nn.Tanh()(self.l2_mu(y1))
-        y = self.l3_mu(y2)
-
-        w_sigma = w[..., self.param_idxs[1]]
-        z1 = nn.Tanh()(self.l1_sig(w_sigma))
-        z2 = nn.Tanh()(self.l2_sig(z1))
-        z = nn.Softplus()(self.l3_sig(z2))
-
-        return y, z
-
-    def forward(self, w, data):
-        y, z = self.decode(w)
-        return torch.mean((data - y) ** 2 / (2 * z ** 2) + torch.log(z), dim=-1)
-
-    def mean(self, params):
-        return params[0]
-
-    def std(self, params):
-        return params[1]
-
-
-class SkewNLL(ModelObject):
-    def __init__(self, x_dim, num_freqs, n):
-        """
-        neural network that takes a vector of sines and cosines and produces a skew-normal distribution with parameters
-        mu, sigma, and alpha (the outputs of the NN). trains using NLL.
-        :param x_dim: number of dimensions spanned by the probability distr
-        :param num_freqs: list. number of frequencies used for each of the 3 parameters: [num_mu, num_sig, num_alpha]
-        :param n: size of NN's second layer
-        """
-        super(SkewNLL, self).__init__(num_freqs)
-
-        self.l1_mu = nn.Linear(2 * self.num_freqs[0], n)
-        self.l2_mu = nn.Linear(n, 64)
-        self.l3_mu = nn.Linear(64, x_dim)
-
-        self.l1_sig = nn.Linear(2 * self.num_freqs[1], n)
-        self.l2_sig = nn.Linear(n, 64)
-        self.l3_sig = nn.Linear(64, x_dim)
-
-        self.l1_a = nn.Linear(2 * self.num_freqs[2], n)
-        self.l2_a = nn.Linear(n, 64)
-        self.l3_a = nn.Linear(64, x_dim)
-
-    def decode(self, w):
-        w_mu = w[..., self.param_idxs[0]]
-        y1 = nn.Tanh()(self.l1_mu(w_mu))
-        y2 = nn.Tanh()(self.l2_mu(y1))
-        y = self.l3_mu(y2)
-
-        w_sigma = w[..., self.param_idxs[1]]
-        z1 = nn.Tanh()(self.l1_sig(w_sigma))
-        z2 = nn.Tanh()(self.l2_sig(z1))
-        z = 1000 * nn.Softplus()(self.l3_sig(z2))  # std should start big to avoid infinite gradients
-
-        w_a = w[..., self.param_idxs[2]]
-        a1 = nn.Tanh()(self.l1_a(w_a))
-        a2 = nn.Tanh()(self.l2_a(a1))
-        a = self.l3_a(a2)
-
-        return y, z, a
-
-    def forward(self, w, data, training_mask):
-        y, z, a = self.decode(w)
-        norm = torch.distributions.normal.Normal(0, 1)
-        return -torch.mean((-(data - y) ** 2 / (2 * z ** 2)) - z.log() + norm.cdf(a * (data - y) / z).log(),
-                           dim=-1)
-
-    def mean(self, params):
-        mu, sigma, alpha = params
-        delta = alpha / (1 + alpha ** 2) ** 0.5
-        return mu + sigma * delta * (2 / np.pi) ** 0.5
-
-    def std(selfs, params):
-        mu, sigma, alpha = params
-        delta = alpha / (1 + alpha ** 2) ** 0.5
-        return sigma * (1 - 2 * delta ** 2 / np.pi) ** 0.5
 
 
 class AlternatingSkewNLL(ModelObject):
@@ -202,7 +97,7 @@ class AlternatingSkewNLL(ModelObject):
 
         return y, z, a
 
-    def forward(self, w, data, training_mask):
+    def forward(self, w, data, training_mask=None):
         mu, sig, alpha = self.decode(w)
         if training_mask is None:
             y = mu
@@ -254,8 +149,6 @@ class AlternatingNormalNLL(ModelObject):
         :param x_dim: dimension of what will be modeled
         :param num_freqs: list of the number of frequencies used to model each parameter: [num_mu, num_sigma]
         :param n: size of 2nd layer of NN
-        :param num_slices: training data will be divided into num_slices slices in time, and they will alternate between
-                           being used to train mu and sigma
         """
         super(AlternatingNormalNLL, self).__init__(num_freqs)
 
@@ -281,7 +174,7 @@ class AlternatingNormalNLL(ModelObject):
 
         return y, z
 
-    def forward(self, w, data, training_mask):
+    def forward(self, w, data, training_mask=None):
         mu, sig = self.decode(w)
         if training_mask is None:
             y = mu
@@ -293,10 +186,59 @@ class AlternatingNormalNLL(ModelObject):
 
         losses = (data - y) ** 2 / (2 * z ** 2) + torch.log(z)
         avg = torch.mean(losses, dim=-1)
-        return avg * torch.repeat_interleave(torch.linspace(0.5, 1.5, losses.shape[0])[:, None], avg.shape[-1], 1)
+        return avg
 
     def mean(self, params):
         return params[0]
 
     def std(self, params):
         return params[1]
+
+
+class GammaNLL(ModelObject):
+
+    def __init__(self, x_dim, num_freqs, n):
+        """
+        Negative Log Likelihood neural network assuming Gamma distribution of x at every point in time.
+        Trains using NLL
+        :param x_dim: dimension of what will be modeled
+        :param num_freqs: list of the number of frequencies used to model each parameter: [num_rate, num_a]
+        :param n: size of 2nd layer of NN
+        """
+        super(GammaNLL, self).__init__(num_freqs)
+
+        self.l1_rate = nn.Linear(2 * self.num_freqs[0], n)
+        self.l2_rate = nn.Linear(n, 64)
+        self.l3_rate = nn.Linear(64, x_dim)
+
+        self.l1_a = nn.Linear(2 * self.num_freqs[1], n)
+        self.l2_a = nn.Linear(n, 64)
+        self.l3_a = nn.Linear(64, x_dim)
+
+    def forward(self, w, data, training_mask=None):
+        assert (training_mask is None), "Training masks won't help when using a Gamma distribution"
+
+        rate, a = self.decode(w)
+
+        losses = -torch.distributions.gamma.Gamma(a, rate).log_prob(data)
+        avg = torch.mean(losses, dim=-1)
+        return avg
+
+    def decode(self, w):
+        w_rate = w[..., self.param_idxs[0]]
+        rate1 = nn.Tanh()(self.l1_rate(w_rate))
+        rate2 = nn.Tanh()(self.l2_rate(rate1))
+        rate = 1 / nn.Softplus()(self.l3_rate(rate2))  # helps convergence
+
+        w_a = w[..., 2 * self.num_freqs[0]:2 * self.num_freqs[0] + 2 * self.num_freqs[1]]
+        a1 = nn.Tanh()(self.l1_a(w_a))
+        a2 = nn.Tanh()(self.l2_a(a1))
+        a = nn.Softplus()(self.l3_a(a2))
+
+        return rate, a
+
+    def mean(self, params):
+        return params[1] / params[0]
+
+    def std(self, params):
+        return np.sqrt(params[1] / params[0] ** 2)
